5bd2ecae5ff377fd28e5be943b4a7d120adfe3cf
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 21:02:29 +0330

    update: request repository

--- internal/repositories/requests/requests.go
+++ internal/repositories/requests/requests.go
@@ -1,4 +1,32 @@
 package requests
 
+import (
+	"github.com/ceit-aut/policeman/internal/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type Repository interface {
+	GetAll(url string) []model.Request
+	Insert(request model.Request) error
+}
+
+type repository struct {
+	db *mongo.Database
+}
+
+// New generates a new repository interface.
+func New(db *mongo.Database) Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+// GetAll requests.
+func (r *repository) GetAll(url string) []model.Request {
+	return nil
+}
+
+// Insert a new request.
+func (r *repository) Insert(request model.Request) error {
+	return nil
 }
