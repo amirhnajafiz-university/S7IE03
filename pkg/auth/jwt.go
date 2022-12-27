1811c6a3eb4eb2c18555b884c2e9828cc1519a65
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 20:03:28 +0330

    fix: jwt methods

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,9 +1,15 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+)
+
+var (
+	errSigningMethod = errors.New("error in signing method")
+	errInvalidToken  = errors.New("token is invalid")
 )
 
 type Auth struct {
@@ -29,4 +35,28 @@ func (a *Auth) GenerateJWT(username string, password string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+// ParseJWT gets a token string and extracts the data.
+func (a *Auth) ParseJWT(tokenString string) (string, string, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			return "", errSigningMethod
+		}
+
+		return a.Key, nil
+	})
+	if err != nil {
+		return "", "", err
+	}
+
+	// taking out claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username := claims["username"].(string)
+		password := claims["password"].(string)
+
+		return username, password, nil
+	}
+
+	return "", "", errInvalidToken
 }
