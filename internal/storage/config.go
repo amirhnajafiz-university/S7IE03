9064d0ce9955b48df09f2642da53e01a5e7be98a
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 20:17:26 +0330

    add: mongodb storage

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -1,1 +1,9 @@
 package storage
+
+// Config
+// mongodb config parameters.
+type Config struct {
+	Database string `koanf:"database"`
+	Host     string `koanf:"host"`
+	Port     int    `koanf:"port"`
+}
--- internal/storage/mongo.go
+++ internal/storage/mongo.go
@@ -1,1 +1,34 @@
 package storage
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// NewConnection
+// opens a new connection to mongodb database.
+func NewConnection(cfg Config) (*mongo.Database, error) {
+	// mongodb server options
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	mongoURI := fmt.Sprintf("mongo://%s:%d", cfg.Host, cfg.Port)
+
+	clientOptions := options.Client().
+		ApplyURI(mongoURI).
+		SetServerAPIOptions(serverAPIOptions)
+
+	// creating mongodb client
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("mongoDB connection failed: %w", err)
+	}
+
+	// ping mongodb
+	if er := client.Ping(context.TODO(), nil); er != nil {
+		return nil, fmt.Errorf("mongoDB ping failed: %w", er)
+	}
+
+	return client.Database(cfg.Database), nil
+}
