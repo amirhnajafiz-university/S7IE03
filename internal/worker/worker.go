ad044c6f50db1fa41a7da9e0d313dfa8028870fc
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 21:48:19 +0330

    update: worker task

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,8 +1,12 @@
 package worker
 
 import (
+	"log"
+	"sync"
 	"time"
 
+	"github.com/ceit-aut/policeman/internal/client"
+	"github.com/ceit-aut/policeman/internal/model"
 	"github.com/ceit-aut/policeman/internal/repositories"
 )
 
@@ -24,11 +28,39 @@ func New(r repositories.Repositories, timeout int) *Worker {
 // Start worker for monitoring endpoints.
 func (w *Worker) Start() error {
 	for {
+		// create wait group
+		wg := sync.WaitGroup{}
+
 		// get all endpoints
+		endpoints := w.repositories.Endpoints.GetAll()
+
 		// make http request for all
-		// check the errors
-		// save the request into mongodb
+		for _, endpoint := range endpoints {
+			wg.Add(1)
+
+			go func(ep model.Endpoint) {
+				w.do(ep)
+
+				wg.Done()
+			}(endpoint)
+		}
+
+		// wait for all requests
+		wg.Wait()
+
+		log.Println("all endpoints are update now")
 
 		time.Sleep(time.Duration(w.timeout) * time.Minute)
+	}
+}
+
+// do will check a http endpoint.
+func (w *Worker) do(endpoint model.Endpoint) {
+	if req, err := client.MakeHTTPRequest(endpoint); err != nil {
+		log.Printf("make http request for endpoint failed:\n\t%v\n", err)
+	} else {
+		if e := w.repositories.Requests.Insert(*req); e != nil {
+			log.Printf("failed to store request:\n\t%v\n", e)
+		}
 	}
 }
