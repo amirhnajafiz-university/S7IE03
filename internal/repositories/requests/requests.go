678f813c1c36e6a7e53d12b476e971ce21709040
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 21:22:00 +0330

    add: requests repo methods

--- internal/repositories/requests/requests.go
+++ internal/repositories/requests/requests.go
@@ -1,9 +1,14 @@
 package requests
 
 import (
+	"context"
+
 	"github.com/ceit-aut/policeman/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+const collectionName = "requests"
 
 // Repository manages the requests models.
 type Repository interface {
@@ -22,12 +27,37 @@ func New(db *mongo.Database) Repository {
 	}
 }
 
-// GetAll requests.
+// GetAll requests of an endpoint.
 func (r *repository) GetAll(url string) []model.Request {
-	return nil
+	var (
+		requests []model.Request
+		request  model.Request
+
+		ctx    = context.Background()
+		filter = bson.M{"url": url}
+
+		collection = r.db.Collection(collectionName)
+	)
+
+	if cursor, err := collection.Find(ctx, filter); err == nil {
+		for cursor.Next(ctx) {
+			if er := cursor.Decode(&request); er == nil {
+				requests = append(requests, request)
+			}
+		}
+	}
+
+	return requests
 }
 
 // Insert a new request.
 func (r *repository) Insert(request model.Request) error {
-	return nil
+	var (
+		ctx        = context.Background()
+		collection = r.db.Collection(collectionName)
+	)
+
+	_, err := collection.InsertOne(ctx, request)
+
+	return err
 }
