5870d07e98f5356d95e91095bb4b99ebee411129
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-27 21:08:21 +0330

    add: new method for repositories

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ceit-aut/policeman/internal/repositories/endpoints"
 	"github.com/ceit-aut/policeman/internal/repositories/requests"
 	"github.com/ceit-aut/policeman/internal/repositories/users"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Repositories manages to keep the models repositories.
@@ -11,4 +12,13 @@ type Repositories struct {
 	Users     users.Repository
 	Endpoints endpoints.Repository
 	Requests  requests.Repository
+}
+
+// New returns a repositories' struct.
+func New(db *mongo.Database) *Repositories {
+	return &Repositories{
+		Users:     users.New(db),
+		Endpoints: endpoints.New(db),
+		Requests:  requests.New(db),
+	}
 }
