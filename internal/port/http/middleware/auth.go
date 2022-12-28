09fcb6c98223a8e02983b1965ffdd95ea955e265
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-28 17:14:07 +0330

    add: auth middleware

--- internal/port/http/middleware/auth.go
+++ internal/port/http/middleware/auth.go
@@ -6,17 +6,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Authentication is used to check users tokens in header.
 func (m *Middleware) Authentication(ctx *fiber.Ctx) error {
 	if token, ok := ctx.GetReqHeaders()["token"]; ok {
 		if user, pass, err := m.Auth.ParseJWT(token); err == nil {
+			// set variables into request context
 			ctx.Locals("username", user)
 			ctx.Locals("password", pass)
 
 			return ctx.Next()
 		} else {
+			// 401
 			return ctx.SendStatus(http.StatusUnauthorized)
 		}
 	} else {
+		// 400
 		return ctx.SendStatus(http.StatusBadRequest)
 	}
 }
