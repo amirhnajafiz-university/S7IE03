254caa182e51a9a5a5990571e31db1ded9765e62
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-28 17:28:00 +0330

    add: request types

--- internal/port/http/request/endpoint.go
+++ internal/port/http/request/endpoint.go
@@ -1,1 +1,5 @@
 package request
+
+type EndpointInfo struct {
+	Address string `json:"address"`
+}
--- internal/port/http/request/user.go
+++ internal/port/http/request/user.go
@@ -1,1 +1,6 @@
 package request
+
+type UserInfo struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
