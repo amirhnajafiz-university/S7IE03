1ee9907ecb6a40269967ca94ce32e474f14ed4e8
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-28 15:18:19 +0330

    update: worker and pool

---
+++ internal/worker/pool.go
@@ -0,0 +1,49 @@
+package worker
+
+import (
+	"log"
+
+	"github.com/ceit-aut/policeman/internal/client"
+	"github.com/ceit-aut/policeman/internal/model"
+	"github.com/ceit-aut/policeman/internal/repositories"
+)
+
+// innerWorker manages to do the tasks.
+type innerWorker struct {
+	repositories repositories.Repositories
+	channel      chan model.Endpoint
+}
+
+// newPool generates a new worker pool.
+func newPool(capacity int, channel chan model.Endpoint, r repositories.Repositories) {
+	for i := 0; i < capacity; i++ {
+		iw := innerWorker{
+			channel:      channel,
+			repositories: r,
+		}
+
+		go iw.listen()
+	}
+}
+
+// listen for events and send them to workers.
+func (i *innerWorker) listen() {
+	for {
+		// listen on channel
+		ep := <-i.channel
+
+		// make http request
+		i.do(ep)
+	}
+}
+
+// do will check a http endpoint.
+func (i *innerWorker) do(endpoint model.Endpoint) {
+	if req, err := client.MakeHTTPRequest(endpoint); err != nil {
+		log.Printf("make http request for endpoint failed:\n\t%v\n", err)
+	} else {
+		if e := i.repositories.Requests.Insert(*req); e != nil {
+			log.Printf("failed to store request:\n\t%v\n", e)
+		}
+	}
+}
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,11 +1,8 @@
 package worker
 
 import (
-	"log"
-	"sync"
 	"time"
 
-	"github.com/ceit-aut/policeman/internal/client"
 	"github.com/ceit-aut/policeman/internal/model"
 	"github.com/ceit-aut/policeman/internal/repositories"
 )
@@ -15,52 +12,35 @@ import (
 type Worker struct {
 	repositories repositories.Repositories
 	timeout      int
+	workers      int
 }
 
 // New builds a new repositories' struct.
-func New(r repositories.Repositories, timeout int) *Worker {
+func New(r repositories.Repositories, timeout int, workers int) *Worker {
 	return &Worker{
 		repositories: r,
 		timeout:      timeout,
+		workers:      workers,
 	}
 }
 
 // Start worker for monitoring endpoints.
 func (w *Worker) Start() error {
-	for {
-		// create wait group
-		wg := sync.WaitGroup{}
+	// create a channel
+	channel := make(chan model.Endpoint)
 
+	// create a new worker pool
+	newPool(w.workers, channel, w.repositories)
+
+	for {
 		// get all endpoints
 		endpoints := w.repositories.Endpoints.GetAll()
 
-		// make http request for all
+		// start worker for each endpoint
 		for _, endpoint := range endpoints {
-			wg.Add(1)
-
-			go func(ep model.Endpoint) {
-				w.do(ep)
-
-				wg.Done()
-			}(endpoint)
+			channel <- endpoint
 		}
-
-		// wait for all requests
-		wg.Wait()
-
-		log.Println("all endpoints are update now")
 
 		time.Sleep(time.Duration(w.timeout) * time.Minute)
-	}
-}
-
-// do will check a http endpoint.
-func (w *Worker) do(endpoint model.Endpoint) {
-	if req, err := client.MakeHTTPRequest(endpoint); err != nil {
-		log.Printf("make http request for endpoint failed:\n\t%v\n", err)
-	} else {
-		if e := w.repositories.Requests.Insert(*req); e != nil {
-			log.Printf("failed to store request:\n\t%v\n", e)
-		}
 	}
 }
