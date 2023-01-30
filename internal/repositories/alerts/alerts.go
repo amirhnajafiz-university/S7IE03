e086456113ac082d0c2325c49ba18b9c06cbd4a6
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-30 20:28:35 +0330

    add: get all alerts of an endpoints

--- internal/repositories/alerts/alerts.go
+++ internal/repositories/alerts/alerts.go
@@ -1,7 +1,9 @@
 package alerts
 
 import (
+	"context"
 	"github.com/ceit-aut/S7IE03/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -10,7 +12,7 @@ const collectionName = "alerts"
 
 // Repository manages the alerts models
 type Repository interface {
-	GetAllForEndpoint(id string) []model.Alert
+	GetAll(id string) []model.Alert
 	Insert(alert model.Alert) error
 }
 
@@ -25,8 +27,27 @@ func New(db *mongo.Database) Repository {
 	}
 }
 
-func (r *repository) GetAllForEndpoint(id string) []model.Alert {
+// GetAll alerts of an endpoint.
+func (r *repository) GetAll(id string) []model.Alert {
+	var (
+		alerts []model.Alert
+		alert  model.Alert
 
+		ctx    = context.Background()
+		filter = bson.M{"endpoint_id": id}
+
+		collection = r.db.Collection(collectionName)
+	)
+
+	if cursor, err := collection.Find(ctx, filter); err != nil {
+		for cursor.Next(ctx) {
+			if er := cursor.Decode(&alert); er == nil {
+				alerts = append(alerts, alert)
+			}
+		}
+	}
+
+	return alerts
 }
 
 func (r *repository) Insert(alert model.Alert) error {
