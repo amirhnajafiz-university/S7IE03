6e2ea8a4da0fbcd57005b4d80b0a77d9d098004b
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-30 20:30:07 +0330

    add: alerts repository in all repositories

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/ceit-aut/S7IE03/internal/repositories/alerts"
 	"github.com/ceit-aut/S7IE03/internal/repositories/endpoints"
 	"github.com/ceit-aut/S7IE03/internal/repositories/requests"
 	"github.com/ceit-aut/S7IE03/internal/repositories/users"
@@ -10,6 +11,7 @@ import (
 
 // Repositories manages to keep the models repositories.
 type Repositories struct {
+	Alerts    alerts.Repository
 	Users     users.Repository
 	Endpoints endpoints.Repository
 	Requests  requests.Repository
@@ -18,6 +20,7 @@ type Repositories struct {
 // New returns a repositories' struct.
 func New(db *mongo.Database) *Repositories {
 	return &Repositories{
+		Alerts:    alerts.New(db),
 		Users:     users.New(db),
 		Endpoints: endpoints.New(db),
 		Requests:  requests.New(db),
