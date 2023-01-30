16b0c05ff22d1e066502db6e84b9f7d544ffed83
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-30 20:33:32 +0330

    feat: create alert for failed requests

--- internal/worker/inner.go
+++ internal/worker/inner.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"log"
+	"time"
 
 	"github.com/ceit-aut/S7IE03/internal/client"
 	"github.com/ceit-aut/S7IE03/internal/model"
@@ -35,6 +36,20 @@ func (i *innerWorker) do(endpoint model.Endpoint) {
 		} else {
 			if !(req.Code >= 200 && req.Code < 300) {
 				endpoint.FailedTimes++
+
+				// create alert for failed endpoints
+				if endpoint.FailedTimes == endpoint.Threshold {
+					al := model.Alert{
+						CreateTime: time.Now(),
+						EndpointId: endpoint.ID.Hex(),
+						Errors:     endpoint.Threshold,
+					}
+					if er := i.repositories.Alerts.Insert(al); er != nil {
+						log.Printf("failed to store alert: \n\t%v\n", er)
+					}
+
+					endpoint.FailedTimes = 0
+				}
 
 				if er := i.repositories.Endpoints.Update(endpoint); er != nil {
 					log.Printf("failed to update request: \n\t%v\n", e)
